c1f8017e58defbe79849369af115ff63271b1a58
cjosue (ubuntu) <dev.carlos.morales@gmail.com>
2026-03-08 20:56:37 -0500

    Refactor code structure for improved readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg', 'kapiwallet.png'],
       manifest: {
         name: 'KapiWallet',
         short_name: 'KapiWallet',
@@ -19,12 +19,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'kapiwallet.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'kapiwallet.png',
             sizes: '512x512',
             type: 'image/png'
           }
